d8552fcf89dc16536a31db6c8c6b86c22f4332fd
CarlygameDev <carlyel322@gmail.com>
2023-10-03 13:05:13 -0400

    added Player Attributes, Health and Speed

---
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/PlayerScript/PlayerAttributes.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+    // use the inspector to assign a value
+    public int playerHealth;
+    public int playerSpeed;
+
+    public int punchDamage;
+    public float punchCooldown;
+
+    public int rocketDamage;
+    public int rocketSpeed;
+    public float rocketCooldown;
+
+
+  
+}
+
+
+
+
+
+
+
