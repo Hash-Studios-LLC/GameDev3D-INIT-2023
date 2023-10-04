77f40c3a6236d0e0468a271989124ba2ffe084df
MasterShakezula <90061079+MasterShakezula@users.noreply.github.com>
2023-10-04 16:31:17 -0400

    Mock UI scene + Camera script
    Created a mockup scene for the main menu UI (just a mockup for UI practicing) and a mockup camera script so the main menu screen's camera will rotate around the setting (like minecraft)

---
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/Camera Menu Script/CameraMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Camera camera;
+    // with time delta and time sync to change speed
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
