039cc2f1041ef919aa40f3f89178def77c7d5846
Andres Agudelo <andyagudelo42@gmail.com>
2023-10-10 14:20:36 -0400

    Added some functionality to the AttackPlayerInput script

--- Assets/_CURRENT_PROJECT/Programing & UI Department/Core/Player/AttackPlayerInput.cs
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/Core/Player/AttackPlayerInput.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class AttackPlayerInput : MonoBehaviour
 {
+    //area in which attack happens
+    private GameObject punchArea = default;
+
+    private bool playerPunching = false;
     
+    private float timeToPunch = 0.25f;
+    private float timer = 0f;
+
+    void Start()
+    {
+        punchArea = transform.GetChild(0).gameObject;
+    }
+
+
     // Update is called once per frame
     void Update()
-    {
+    {   
+        //binds player punch to the key Q
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            playerPunch();
+        }
 
+
+        if(playerPunching) {
+            timer += Time.deltaTime;
+
+            //resets the punch attack
+            if(timer >= timeToPunch)
+            {
+                timer = 0;
+                playerPunching = false;
+                punchArea.SetActive(playerPunching)
+            }
+        }
+
+        
+    }
+
+    private void playerPunch()
+    {
+        playerPunching = true;
+        punchArea.SetActive(playerPunching)
     }
 }
