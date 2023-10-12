31594e16262a53ac29869a1341c2a73834289915
Alessandra Ortega Arrieta <aleortega2000@gmail.com>
2023-10-12 18:14:47 -0400

    SettingsMenu Resolution and Quality Options in progress

--- Assets/_CURRENT_PROJECT/Programing & UI Department/UI/SettingsScreen/SettingsScene.cs
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/UI/SettingsScreen/SettingsScene.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SettingsScene : MonoBehaviour
+public class SettingsScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    List<string> resolutionList = new List<string>();
+    List<string> graphicsQualityList = new List<string>();
+
+    public void LoadSettingsMenu()
     {
-        
+        SceneManager.LoadScene("SettingsScreen");
+        // Call the functions to populate the lists
+        addResolutions(resolutionList);
+        addGraphicsQuality(graphicsQualityList);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetVolume(float volume)
     {
-        
+        Debug.Log(volume);
+    }
+
+
+    void changeResolution()
+    {
+
+    }
+
+    List<string> addResolutions(List<string> resolutionList) //returns a list of the resolution options 
+    {
+        resolutionList.Add("1920 x 1080");
+        resolutionList.Add("2560 x 1440");
+        resolutionList.Add("3840 x 2160");
+        resolutionList.Add("7680 x 4320");
+
+        return resolutionList;
+    }
+
+    List<string> addGraphicsQuality(List<string> graphicsQualityList) // returns a list of the graphics quality options
+    {
+        graphicsQualityList.Add("Low");
+        graphicsQualityList.Add("Medium");
+        graphicsQualityList.Add("High");
+        return graphicsQualityList;
     }
 }
