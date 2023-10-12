0123efa7659199f08f98f9c493c4362567b703c8
Esau Herrera <90202949+eherrera1212@users.noreply.github.com>
2023-10-12 17:57:11 -0400

    Fixes to AttackPlayerInput
    there are still issues with settings screen I'm gonna work on that

--- Assets/_CURRENT_PROJECT/Programing & UI Department/UI/SettingsScreen/SettingsScene.cs
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/UI/SettingsScreen/SettingsScene.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SettingsScene : MonoBehaviour
+public class SettingsScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    List<string> resolutionList = new List<string>();
+    List<string> graphicsQualityList = new List<string>();
+
+    public void LoadSettingsMenu()
     {
-        
+        SceneManager.LoadScene("SettingsScreen");
+        // Call the functions to populate the lists
+        addResolutions(resolutionList);
+        addGraphicsQuality(graphicsQualityList);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void SetVolume(float volume)
     {
-        
+        Debug.Log(volume);
+    }
+
+    List<string> addResolutions(List<string> resolutionList) //returns a list of the resolution options 
+    {
+        Resolution[] resolutions = SettingsScreen.resolutions;
+
+        foreach (Resolution resolution in resolutions)
+        {
+            string option = resolution.width + "x" + resolution.height;
+            resolutionList.Add(option);
+        }
+        return resolutionList;
+    }
+
+    List<string> addGraphicsQuality(List<string> graphicsQualityList) // returns a list of the graphics quality options
+    {
+        graphicsQualityList.Add("placeholder1");
+        graphicsQualityList.Add("placeholder2");
+        graphicsQualityList.Add("placeholder3");
+        graphicsQualityList.Add("placeholder4");
+        graphicsQualityList.Add("placeholder5");
+        graphicsQualityList.Add("placeholder6");
+        return graphicsQualityList;
     }
 }
