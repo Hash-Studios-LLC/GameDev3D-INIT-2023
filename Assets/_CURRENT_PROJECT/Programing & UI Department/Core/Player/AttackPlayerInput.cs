f3ea048494f0e159082b417362b003b8d0aabe7b
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2023-10-12 22:07:03 -0400

    Completed pause menu and manager
    implement pause menu and pause menu manager script.
    
    also added y/t submit controls to the default input system

--- Assets/_CURRENT_PROJECT/Programing & UI Department/Core/Player/AttackPlayerInput.cs
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/Core/Player/AttackPlayerInput.cs
@@ -8,13 +8,9 @@ public class AttackPlayerInput : MonoBehaviour
     private GameObject punchArea = default;
 
     private bool playerPunching = false;
-    private bool playerShooting = false;
     
-    private float timeToShoot = 0.15f;
-    private float timerForShoot = 0f;
-
     private float timeToPunch = 0.25f;
-    private float timerForPunch = 0f;
+    private float timera = 0f;
 
     void Start()
     {
@@ -26,18 +22,18 @@ public class AttackPlayerInput : MonoBehaviour
     void Update()
     {   
         //binds player punch to the key Q
-        if(Input.GetButton("Punch")) {
+        if(Input.GetKeyDown(KeyCode.Q)) {
             playerPunch();
         }
 
 
         if(playerPunching) {
-            timer += Time.deltaTime;
+            timera += Time.deltaTime;
 
             //resets the punch attack
-            if(timer >= timeToPunch)
+            if(timera >= timeToPunch)
             {
-                timer = 0;
+                timera = 0;
                 playerPunching = false;
                 punchArea.SetActive(playerPunching);
             }
