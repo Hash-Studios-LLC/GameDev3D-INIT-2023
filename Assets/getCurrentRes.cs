b40f4a02a57d7d6d2d9954402ab9cf9899633eb8
Ivan Figueroa <Ivanfiggomez@gmail.com>
2023-10-13 01:11:16 -0400

    Fixed the positionion of the settings menu buttons and more...
    added script to get current resolution and display it in the resolution label.
    Added 3 different audio mixers and synced them up with the sliders in the settings menu..

---
+++ Assets/getCurrentRes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class getCurrentRes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private TMP_Text currentRes;
+    void Start()
+    {
+        currentRes = GetComponent<TMP_Text>();
+        currentRes.text = Screen.currentResolution.ToString();
+    }
+
+    // Update is called once per frame
+}
