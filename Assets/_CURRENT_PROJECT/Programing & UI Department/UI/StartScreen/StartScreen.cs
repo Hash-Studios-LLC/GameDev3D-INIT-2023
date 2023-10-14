2129473ecd169e4ad931f14c2eaf785585b6c5c2
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2023-10-14 14:16:43 -0400

    Minor Improvements to the start scene
    reorganized hierarchy, changed panel sprite, added log statements to startScreen Script.

--- Assets/_CURRENT_PROJECT/Programing & UI Department/UI/StartScreen/StartScreen.cs
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/UI/StartScreen/StartScreen.cs
@@ -7,14 +7,19 @@ public class StartScreen : MonoBehaviour
 {
     //TO-DO: Make the Start Screen functionalities:
     //For the Start game button
+
     public void StartGame()
     {
+        Debug.Log("Loading to fighting menu...");
+
         SceneManager.LoadScene("Fighter Menu");
     }
   
     //For the Settings button
     public void GoToSettings()
     {
+        Debug.Log("Loading to setting screen...");
+
         SceneManager.LoadScene("SettingsScreen");
     }
 
