684938f98ee3bc4ce15ee2dcf38483dcc7c53bb1
Woodhouse3D <45299674+FloridaChristopher@users.noreply.github.com>
2023-10-15 15:43:36 -0400

    Implimented boilerplate bullet stuff
    - implemented basic bullet code.
    - bullet now deletes itself on contact or after a certain amount of time.
    - bullet has damage value.

---
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/Core/Player/Bullet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Dependencies")]
+
+
+    [Header("Options")]
+    [SerializeField] private float lifespan = 2f; //length of time untill bullet despawns
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BulletLifespan());
+    }
+
+
+    IEnumerator BulletLifespan() // this function waits some amount of time and then deletes the bullet
+    {
+        yield return new WaitForSeconds(lifespan);
+        destroyProjectile();
+    }
+
+    void destroyProjectile() // this function is here in case you want to add any stuff to the destroy behavior.
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit object");
+        Destroy(this.gameObject);
+    }
+
+}
