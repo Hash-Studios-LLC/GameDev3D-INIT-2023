8d6f3d1c5f82a4c045412d1364616ea7a6b4a492
Carlos Cruz <carlyel322@gmail.com>
2023-11-13 01:39:02 -0500

    hp value next to bar

--- Assets/_CURRENT_PROJECT/Programing & UI Department/UI/InGameOverlay/HPbar.cs
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/UI/InGameOverlay/HPbar.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class HPbar : MonoBehaviour
 {
-
+    int maxHp;
     public Slider slider;
-
+    public TextMeshProUGUI hp;
     public void setHealth(int health)
     {
         slider.value = health;
+        hp.text = health.ToString() + "/" + maxHp.ToString();
     }
 
     public void setMaxHealth(int health)
     {
+        maxHp = health;
         slider.maxValue = health;
         slider.value = health;
+        hp.text =health.ToString()+"/" + health.ToString();
     }
 }
