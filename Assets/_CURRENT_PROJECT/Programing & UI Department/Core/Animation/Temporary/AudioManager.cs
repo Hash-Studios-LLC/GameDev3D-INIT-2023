8b13cb61434bb690dfc7228d74835b00afb68a2e
Daniel J <djalali39@protonmail.com>
2023-11-17 13:40:47 -0500

    Fixed Audio Manager
    Fixed Audio Manager

--- Assets/_CURRENT_PROJECT/Programing & UI Department/Core/Animation/Temporary/AudioManager.cs
+++ Assets/_CURRENT_PROJECT/Programing & UI Department/Core/Animation/Temporary/AudioManager.cs
@@ -14,7 +14,7 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
         foreach(Sound s in sounds)
         {
           s.source=gameObject.AddComponent<AudioSource>();
